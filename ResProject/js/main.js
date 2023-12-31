b898cbe0b2c3dfca3560ebbf6a3549d4fa3ccecf
MsKat04 <116561169+MsKat04@users.noreply.github.com>
2023-12-31 15:21:17 +0500

    Add files via upload

--- ResProject/js/main.js
+++ ResProject/js/main.js
@@ -1,152 +1,222 @@
-const swiper = new Swiper(
-    '.swiper-container',{
-        loop: true,
-        navigation:{
-            nextEl:'.slider-button-next',
-            prevEl: '.slider-button-prev'
-        }
-    }
-);
+function dataToJson(data){
+    return JSON.stringify(data);
+};
+
+function jsonToData(data){ 
+    return JSON.parse(data);
+};
+
+function getCartData(){
+    return localStorage.getItem('cartData');
+};
+
+function setCartData(data){
+    localStorage.setItem('cartData', data);
+};
+
+function getMyCart(){
+    let cart = localStorage.getItem('cartData');
+    cart = cart ? jsonToData(cart) : [];
+    return cart;
+}
 
 let allGoods = [];
 
 function getGoods(){
-    fetch('json/inf.json').then(res => res.json()).then(result => {allGoods = result});
+    fetch('db/db.json').then(res => res.json()).then(result => {allGoods = result;})
 }
 
+const cart = { 
+    cartGoods: getMyCart(),
 
-//корзина поля и методы там
-const cart ={
-    cartGoods:[], //массив объектов, будем добовлять в корзину
     addCartId(id){
-        const goodsItems = this.cartGoods.find(good => good.id === id);
-        if(goodsItems){
+        const goodItem = this.cartGoods.find(good => good.id === id);
+        if (goodItem){
             this.plusGood(id)
-        } else{
-            const {id: idx, name, price} = allGoods.find(good => good.id === id)
-            this.cartGoods.push({id: idx, name, price, count:1});
+        }else{
+            const {id: idx,name,price} = allGoods.find(good => good.id === id);
+            this.cartGoods.push({id: idx,name,price, count: 1});
             this.cartRender();
-        }
+        } 
+        setCartData(dataToJson(this.cartGoods));
 
     },
     cartRender(){
-        tableCard.textContent ='';
-        this.cartGoods.forEach(({id, name, price, count}) =>{
-            const tr = document.createElement('tr');
-            tr.className = 'cart-item'
-            tr.dataset.id = id;
-            tr.innerHTML = `
-            <td> ${name} </td>
-            <td> ${price} </td>
-            <td><button class = "mines" data-id = ${id}>-</button></td>
-            <td> ${count} </td>
-            <td><button class = "plus" data-id = ${id}>+</button></td>
-            <td> ${count * price} </td>
-            <td><button class = "delete" data-id = ${id}>X</button></td>`
-
-            tableCard.append(tr);
+        cartTableGoods.textContent = '';
+        this.cartGoods.forEach(({name, id, price, count}) => {
+            const trGood = document.createElement('tr');
+            trGood.className = 'cart-item';
+            trGood.dataset.id = id;
+            trGood.innerHTML = `
+            <td>${name}</td>
+            <td>${price}\ ₽</td>
+            <td><button class = "cart-btn-minus new-year-btn" data-id = ${id}>-</button></td>
+            <td>${count}</td>
+            <td><button class = "cart-btn-plus new-year-btn" data-id = ${id}>+</button></td>
+            <td>${count * price}</td>
+            <td><button class = "cart-btn-delete new-year-btn" data-id = ${id}>x</button></td>
+            `
+            cartTableGoods.append(trGood);
         })
-        const totalPrice = this.cartGoods.reduce((sum,item) => sum +item.price * item.count, 0);
-        total.textContent = `${totalPrice}`;
-        //*
+        const totalPrice = this.cartGoods.reduce((sum, item) => sum + item.price * item.count, 0);
+        cartTableTotal.textContent = `${totalPrice}₽`;
+        cartCount.textContent = `${totalPrice}₽`;
     },
     plusGood(id){
         const elem = this.cartGoods.find(el => el.id === id);
         if(elem){
             elem.count++;
         }
-        this.cartRender();
+        setCartData(dataToJson(this.cartGoods));
+        this.cartRender(); //перерисовка
     },
+
     minusGood(id){
-        const elem = this.cartGoods.find(good => good.id == id);
-        if(elem.count === 1){
+        const elem = this.cartGoods.find(el => el.id === id);
+        if (elem.count === 1) {
             this.deleteGood(id);
-        } else{
+        }else{
             elem.count--;
         }
+        setCartData(dataToJson(this.cartGoods));
         this.cartRender();
     },
+
     deleteGood(id){
         this.cartGoods = this.cartGoods.filter(el => el.id !== id);
+        setCartData(dataToJson(this.cartGoods));
         this.cartRender();
     }
-}
-const buttonBuy = document.querySelector('.buy');
-const modalCard = document.querySelector('.overlay');
-const tableCard = document.querySelector('.modal-goods');
-const total = document.querySelector('.modal-sum');
-const navigation = document.querySelectorAll('.navigation-items');
-const logotipe = document.querySelector('.logotipe');
 
-function renderCart(data){
+}
+
+const buttonCart = document.querySelector('.button-cart'); 
+const modalCart = document.querySelector('#modal-cart')
+const cartTableGoods = document.querySelector('.cart-table__goods');
+const cartTableTotal = document.querySelector('.card-table__total');
+const cartCount = document.querySelector('.cart-count');
+const navigationLink = document.querySelectorAll('.navigation-link');
+const longGoodList = document.querySelector('.long-goods-list');
+const specialOffers = document.querySelector('.special-offers'); 
+const logoLink = document.querySelector('.logo-link');
+const categoryTitle = document.querySelector('.category-title');
+const cardMenu = document.querySelector('.card-menu');
+const parents = document.querySelectorAll('.navigation-item');
+const menu = document.querySelector('.menu');
+const menuPresent = document.querySelector('.long-menu-present');
+
+menuPresent.classList.add('blokirovka');
+
+menu.addEventListener('click', ()=>{
+    
+    renderCards(allGoods);
+})
+
+cardMenu.addEventListener('click', () => {
+    specialOffers.classList.add('blokirovka');
+    document.body.classList.add('v');
+    menuPresent.classList.remove('blokirovka');
+})
+
+
+cartTableGoods.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target.tagName === 'BUTTON'){
+        const className = target.className;
+        const id = target.dataset.id;
+        switch(className) {
+            case 'cart-btn-delete new-year-btn':
+                cart.deleteGood(id);
+                break;
+            case 'cart-btn-minus new-year-btn':
+                cart.minusGood(id);
+                break;
+            case 'cart-btn-plus new-year-btn':
+                cart.plusGood(id);
+                break;
+        }
+    }
+})
+
+function renderCards(data){
+    longGoodList.textContent = '';
+    const cards = data.map(good => createCard(good));
+    longGoodList.append(...cards);
     document.body.classList.add('show-goods');
 }
 
-logotipe.addEventListener('click', ()=>{
-    if(document.body.classList.contains('show-goods')){
+logoLink.addEventListener('click', () => {
+    if(document.body.classList.contains('show-goods') || document.body.classList.remove('v')
+    ){
         document.body.classList.remove('show-goods');
+        document.body.classList.remove('v');
+        specialOffers.classList.remove('blokirovka');
     }
+    menuPresent.classList.add('blokirovka');
 })
 
-
-
-function filterCarts(field, value){
-    renderCart(allGoods.filter(good => good[field] === value));
+function createCard(objCard){
+    const card = document.createElement('div');
+    card.className = "col-lg-3 col-sm-6";
+    card.innerHTML = `
+    <div class = "goods-card">
+    <img src="db/${objCard.img}" alt="${objCard.name}" class="good-image">
+    <h3 class="goods-title">${objCard.name}</h3>
+    <button class="button goods-card-btn add-to-card" data-id= "${objCard.id}">
+    <span class="button-price">${objCard.price} ₽</span></button>
+    </div>`;
+    card.addEventListener('click', () =>{
+        cart.addCartId(objCard.id);
+    })
+    return card;
 }
 
-navigation.forEach((link) => {
-    link.addEventListener('click', (e)=>{
-        const fields = link.dataset.field;
-        if(fields){
-            const value = link.textContent;
-            filterCarts(fields,value);
+
+
+function filterCard(field, value){
+    renderCards(allGoods.filter(good => good[field] === value))
+
+}
+
+navigationLink.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        menuPresent.classList.add('blokirovka');
+        parents.forEach((p) => p.classList.remove('navigation-item-active'));
+        const field = link.dataset.field;
+        if (field){ 
+            const value = link.textContent;     
+            categoryTitle.textContent = `${value}`;         
+            filterCard(field, value);
             return;
         }
-        renderCart(allGoods);
+        else{
+            categoryTitle.textContent = 'все меню';
+        }
+        renderCards(allGoods);
+        
     })
 })
 
+buttonCart.addEventListener('click', () => {
+    modalCart.classList.add('show');
+})
 
-//делаем рабочие кнопки
-tableCard.addEventListener('click', (e)=>{
+document.addEventListener('mouseup', (e) => {
     const target = e.target;
-    if(target.tagName === 'BUTTON'){
-        const className = target.className;
-        const id = target.dataset.id;
-
-        switch(className){
-            case 'plus':
-                cart.plusGood(id);
-                break;
-            case 'mines':
-                cart.minusGood(id);
-                break;
-            case 'delete':
-                cart.deleteGood(id);
-                break;
+    if (!target.closest('.modal') || target.classList.contains('modal-close')) {
+        if(modalCart.classList.contains('show')){
+            modalCart.classList.remove('show');
         }
     }
 })
 
-//закрыть окно корзины
-document.addEventListener('mouseup', (e) =>{
-    if(!e.target.closest('.modal') || e.target.classList.contains('modal-btn')){
-        modalCard.classList.remove('show');
+document.body.addEventListener('click', (e) => { 
+    const target = e.target.closest('.add-to-cart');
+    if(target) {
+        cart.addCartId(target.dataset.id)
     }
 })
-//открыть окно
-buttonBuy.addEventListener('click', ()=>{
-    //card.renderCart();
-    modalCard.classList.add('show')
-    
-})
-//добавляем в корзину по клике на кнопку
-document.body.addEventListener('click', (el) =>{
-    const target = el.target.closest('.in-box');
-    if(target){
-        cart.addCartId(target.dataset.id);
-    }
-})
+
+
 getGoods();
-
-
+cart.cartRender();
